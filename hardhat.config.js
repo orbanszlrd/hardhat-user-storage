4b6bfe6310ebcd11358e04ee3681fa4e8198bbf8
Orban Szilard <orbanszlrd@gmail.com>
2022-09-21 00:26:35 +0200

    Add custom task to the hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+const { task } = require('hardhat/config');
+
 require('@nomicfoundation/hardhat-toolbox');
 require('@nomiclabs/hardhat-etherscan');
 require('dotenv').config();
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
