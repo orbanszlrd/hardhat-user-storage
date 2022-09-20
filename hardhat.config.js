16ed82d4848e67fe693c4b8d910998c7924b21bd
Orban Szilard <orbanszlrd@gmail.com>
2022-09-20 23:34:33 +0200

    Add networks to the hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+      chainId: 5,
+    },
+    ganache: {
+      url: process.env.GANACHE_RPC_URL,
+      accounts: [process.env.GANACHE_PRIVATE_KEY],
+      chainId: 1337,
+    },
+  },
   solidity: '0.8.17',
 };
