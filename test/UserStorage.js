2ec2e5c53d13655e6ff8006a611cc3c0f2293906
Orban Szilard <orbanszlrd@gmail.com>
2022-09-21 01:34:57 +0200

    Add test scripts to the package.json

--- test/UserStorage.js
+++ test/UserStorage.js
@@ -4,30 +4,29 @@ const { ethers } = require('hardhat');
 describe('UserStorage', function () {
   let UserStorageFactory, userStorage;
 
+  const user = {
+    name: 'Peter Griffin',
+    age: 50,
+  };
+
   beforeEach(async function () {
     UserStorageFactory = await ethers.getContractFactory('UserStorage');
     userStorage = await UserStorageFactory.deploy();
     await userStorage.deployed();
   });
 
-  it('Should start with count = 0', async function () {
+  it('Should start with count equal to 0', async function () {
     const count = await userStorage.count();
-
     assert.equal(count, 0);
   });
 
-  it('Should start with an empty array', async function () {
+  it('Should start with no users', async function () {
     const users = await userStorage.getAllUsers();
 
     assert.equal(users.length, 0);
   });
 
   it('Should store a new user', async function () {
-    const user = {
-      name: 'Peter Griffin',
-      age: 50,
-    };
-
     await userStorage.addUser(user.name, user.age);
 
     const count = await userStorage.count();
@@ -36,11 +35,6 @@ describe('UserStorage', function () {
   });
 
   it('Should clear the users', async function () {
-    const user = {
-      name: 'Peter Griffin',
-      age: 50,
-    };
-
     let count = 0;
 
     await userStorage.addUser(user.name, user.age);
@@ -55,11 +49,6 @@ describe('UserStorage', function () {
   });
 
   it('Should return the users age', async function () {
-    const user = {
-      name: 'Peter Griffin',
-      age: 50,
-    };
-
     await userStorage.addUser(user.name, user.age);
 
     const age = await userStorage.getAgeByName(user.name);
@@ -68,11 +57,6 @@ describe('UserStorage', function () {
   });
 
   it('Should return the user with the given id', async function () {
-    const user = {
-      name: 'Peter Griffin',
-      age: 50,
-    };
-
     await userStorage.addUser(user.name, user.age);
 
     const userResponse = await userStorage.getUserById(1);
